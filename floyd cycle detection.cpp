6f0c815378e16b8c32f6d8c2247722e338560540
Harsh Sachin Chandekar <59416466+harshchan@users.noreply.github.com>
2021-05-10 21:01:52 +0530

    Update floyd cycle detection.cpp

--- floyd cycle detection.cpp
+++ floyd cycle detection.cpp
@@ -1,5 +1,5 @@
 // 2pointers move at diffrent speeds and if there is a loop in linked list they are xpected to meet somewhere
-bool doesLInkedListHasLoop(node *head)
+bool doesLinkedListHasLoop(node *head)
 {
   node * slowptr=head,*fastptr=head;
   while(slowptr&&fastptr&&fastptr->next)
@@ -12,4 +12,19 @@ bool doesLInkedListHasLoop(node *head)
   }
   return false;             //there is no loop in linked list
   
+}
+//to detect the start of  cycke
+//initialise slowptr to head and fastptr will be at same position as the cycle
+//the point at which they both meet again is the starting point of loop/cycle
+//and now both slowptr and fastptr will move 1 space at a time
+
+if(doesLinkedListHaveLoop(head))
+{
+  slowptr=head;
+  while(slowptr!=fastptr)
+  {
+    fastptr=fastptr->next;
+    slowptr=slowptr->next;
+  }
+  return slowptr;
 }
